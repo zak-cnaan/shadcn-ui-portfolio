8eb3acffa5b64cd98643c01b9d99568dbfa7589c
Zak <izak@moonsite.co.il>
2023-12-20 16:23:31 +0200

    .

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,15 +1,31 @@
+import Header from "../header/Header";
 import { Button } from "../ui/button";
 
 function App() {
   return (
-    <main className="flex flex-col items-center justify-center h-screen container md:mx-auto">
-      <div className="flex items-center space-x-2">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus impedit incidunt iste corporis laborum veniam rerum iusto tempore soluta! Placeat blanditiis sapiente corporis nemo magnam. Cupiditate sequi autem voluptate minima!</p>
-      <Button type="submit">Submit</Button>
-      <Button variant={"secondary"}>Submit</Button>
-        <label htmlFor="airplane-mode">Airplane Mode</label>
-      </div>
-    </main>
+    <div className="flex flex-col min-h-screen ">
+     <Header />
+
+      <main className="flex flex-col flex-grow pb-[120px] relative z-10">
+        <div className="flex items-center space-x-2 bg-[gray] flex-grow min-h-[1200px] justify-start flex-col">
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+              impedit incidunt iste corporis laborum veniam rerum iusto tempore
+              soluta! Placeat blanditiis sapiente corporis nemo magnam.
+              Cupiditate sequi autem voluptate minima!
+            </p>
+            <Button type="submit">Submit</Button>
+            <Button variant={"secondary"}>Submit</Button>
+            <label htmlFor="airplane-mode">Airplane Mode</label>
+          </div>
+        </div>
+      </main>
+
+      <footer className="h-[120px] bg-[red] fixed bottom-0 left-0 w-full z-0">
+        footer
+      </footer>
+    </div>
   );
 }
 
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <header className="sticky top-0 bg-[blue] z-20">
+        <div className="container">header</div>
+      </header>
+    )
+}
+
+export default Header;
